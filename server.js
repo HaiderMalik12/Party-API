d98a110ff4fca16365463712268f7a5ddecb20d2
Haider Malik <haidermalik5044@gmail.com>
2016-01-22 21:00:25 -0800

    Add DELETE/parties/id

--- server.js
+++ server.js
@@ -1,68 +1,78 @@
-var express=require('express');
-var app=express();
-var PORT=process.env.PORT || 4000;
-var _=require('underscore');
-var parties=[];
-var partyNextId=1;
-var bodyParser=require('body-parser');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 4000;
+var _ = require('underscore');
+var parties = [];
+var partyNextId = 1;
+var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.get('/',function(req,res){
-res.send('Hey Express !!');
+app.get('/', function (req, res) {
+    res.send('Hey Express !!');
 });
 
 //GET /parties
 
-app.get('/parties',function(req,res){
- 
- res.json(parties);
+app.get('/parties', function (req, res) {
+
+    res.json(parties);
 
 });
 
 //Get /parties/:id
 
-app.get('/parties/:id',function(req,res){
- var partyId=parseInt(req.params.id,10);
- var matchedParty=_.findWhere(parties,{id:partyId});
- if(matchedParty)
- {
- 	res.json(matchedParty);
- }
- else{
- 	res.status(400).send();
- }
+app.get('/parties/:id', function (req, res) {
+    var partyId = parseInt(req.params.id, 10);
+    var matchedParty = _.findWhere(parties, {id: partyId});
+    if (matchedParty) {
+        res.json(matchedParty);
+    }
+    else {
+        res.status(400).send();
+    }
 
 });
 
 //POST /parties
-app.post('/parties',function(req,res){
+app.post('/parties', function (req, res) {
 
 //validate is there is party Object
- var body=_.pick(req.body,'name','type','credit','debit','balance','city');
-  
-    if(!_.isString(body.name) || body.name.trim().length===0 ||
-    	  !_.isString(body.type) || body.type.trim().length===0 ||
-    	  !_.isString(body.city) || body.city.trim().length===0 ||
-    	  !_.isNumber(body.credit)||
-    	  !_.isNumber(body.debit) ||
-    	  !_.isNumber(body.balance)
-        )
-    {
-    	return res.status(400).send();
+    var body = _.pick(req.body, 'name', 'type', 'credit', 'debit', 'balance', 'city');
+
+    if (!_.isString(body.name) || body.name.trim().length === 0 || !_.isString(body.type) || body.type.trim().length === 0 || !_.isString(body.city) || body.city.trim().length === 0 || !_.isNumber(body.credit) || !_.isNumber(body.debit) || !_.isNumber(body.balance)
+    ) {
+        return res.status(400).send();
     }
 
 
-  body.id=partyNextId++;
-  body.name=body.name.trim();
-  body.type=body.type.trim();
-   
-   parties.push(body);
-  
-  res.json(body);
+    body.id = partyNextId++;
+    body.name = body.name.trim();
+    body.type = body.type.trim();
 
-   });
+    parties.push(body);
 
-app.listen(PORT,function(){
-	console.log('Express is started listening on PORT :' +PORT);
+    res.json(body);
+
+});
+
+//DELETE /parties/:id
+
+app.delete('/parties/:id', function (req, res) {
+    var partyId = parseInt(req.params.id, 10);
+    var matchedParty = _.findWhere(parties, {id: partyId});
+    if (matchedParty) {
+
+        //delete the matched party from parties
+        parties = _.without(parties, matchedParty);
+        res.json(matchedParty);
+    }
+    else {
+        res.status(404).send();
+    }
+
+});
+
+app.listen(PORT, function () {
+    console.log('Express is started listening on PORT :' + PORT);
 });
