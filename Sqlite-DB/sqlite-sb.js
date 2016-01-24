ab18cf76f011a2f228df84d967aeee0ca5b8a4e7
=Haider Malik <haidermalik5044@gmail.com>
2016-01-23 19:48:16 -0800

    Add Party Create and FindBy All ANd FindById using Promises

--- Sqlite-DB/sqlite-sb.js
+++ Sqlite-DB/sqlite-sb.js
@@ -1,6 +1,4 @@
-/**
- * Created by HaiderNazir on 1/23/2016.
- */
+
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('undefined', 'undefined', 'undefined', {
     'dialect': 'sqlite',
@@ -28,8 +26,9 @@ var party = sequelize.define('party',{
     }
 
 });
-sequelize.sync({force:true}).then(function () {
-    console.log('Everything is synced !!');
+sequelize.sync({force:true})
+    .then(function () {
+    console.log('-----------------------Start Create Parties-------------------------------------------');
     party.create({
         name:'Haider Malik',
         type:'Yarn',
@@ -38,6 +37,51 @@ sequelize.sync({force:true}).then(function () {
         debit:1000.0,
         balance:1000.0
     });
-    console.log('Finished !!');
-    console.log(party);
+     return party.create({
+         name:'Shah g',
+         type:'dye',
+         city:'FSD',
+         credit:100.0,
+         debit:100.0,
+         balance:1000.0
+     });
+
+}).then(function(){
+   return party.create({
+       name:'Shah g',
+       type:'dye',
+       city:'FSD',
+       credit:100.0,
+       debit:100.0,
+       balance:1000.0
+   });
+}).then(function(){
+
+    return party.create({
+        name:'Shah g',
+        type:'dye',
+        city:'FSD',
+        credit:100.0,
+        debit:100.0,
+        balance:1000.0
+    });
+}).then(function(){
+    console.log('--------------------Start Find All ----------------------------------');
+    return party.findAll({
+        where:{
+            name:{
+                like:'%Shah g%'
+            }
+        }
+    })
+}).then(function(parties){
+    parties.forEach(function(party){
+        console.log(party.toJSON());
+    });
+    return party.findById('1');
+}).then(function(party){
+    console.log('----------------Start Find By Id -------------------------------------');
+    console.log(party.toJSON());
+}).catch(function(e){
+    console.log(e);
 });
